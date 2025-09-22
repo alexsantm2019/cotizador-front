042d02b95838ef7ca6f821c17794bb568a7717f0
Alexis Muñoz <48456214+alexsantm2019@users.noreply.github.com>
2025-09-22 07:28:33 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,7 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'http://localhost:4200'
+  apiUrl: 'http://localhost:4200',
+  whiteLogo: 'assets/images/logos/logo-white.png',
+  mainLogo: 'assets/images/logos/logo.png'
 };
